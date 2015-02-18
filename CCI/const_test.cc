b46ede3572673e75f0a8604fd708e85ce01079f0
Santiago Garza <sgarza62@yahoo.com>
2015-02-17 16:42:28 -0800

    More puzzles

--- CCI/const_test.cc
+++ CCI/const_test.cc
@@ -3,7 +3,13 @@
 int main() {
   const int x = 10;
   int y = x;
-  std::cout << y << std::endl;
+  std::cout << y << " " << x << std::endl;
   y++;
-  std::cout << y << std::endl;
+  std::cout << y << " " << x << std::endl;
+
+  int z = 10;
+  int a = z;
+  std::cout << a << " " << z << std::endl;
+  a++;
+  std::cout << a << " " << z << std::endl;
 }
